221f8bacc5f819efc8916574447147ed32ad9ea1
bmarvin <beesemarvin@gmail.com>
2022-06-22 17:36:00 +0200

    encoding table can be created for every participant, clustering data for every participant included

--- Main.py
+++ Main.py
@@ -5,18 +5,102 @@ def run_build_encoding_table_dfs(stimulus_object):
     
     encoding_data_a_list = InputHandler.encoding_workflow()
     encoding_data_b_list = InputHandler.encoding_workflow()
-    # TODO: make table for every participant
-    df_table_participant = TableBuilder.build_encoding_table_for_participant(
-        stimulus_object = stimulus_object,
-        vp_nr_a = encoding_data_a_list[0]['vp_nr'],
-        vp_nr_b = encoding_data_b_list[0]['vp_nr'],
-        encoding_faces_a = encoding_data_a_list[0]['df_faces'],
-        encoding_faces_b = encoding_data_b_list[0]['df_faces'],
-        encoding_words_a = encoding_data_a_list[0]['df_words'],
-        encoding_words_b = encoding_data_b_list[0]['df_words'],
-        )
-    return df_table_participant
+    encoding_faces_a_obj = {}
+    encoding_words_a_obj = {}
+    # distinct data dicts for each encoding category
+    for el in encoding_data_a_list:
+        encoding_faces_a_obj[el['vp_nr']] = el['df_faces']
+        encoding_words_a_obj[el['vp_nr']] = el['df_words']
+    encoding_faces_b_obj = {}
+    encoding_words_b_obj = {}
+    for el in encoding_data_b_list:
+        encoding_faces_b_obj[el['vp_nr']] = el['df_faces']
+        encoding_words_b_obj[el['vp_nr']] = el['df_words']
+    
 
+    participants_table_dict = {}
+
+    # creating tables for every participant, clustering each participant into one table
+    for faces_a in list(encoding_faces_a_obj.keys()):
+        encoding_faces_a, encoding_faces_b, encoding_words_a, encoding_words_b = None, None, None, None
+        vp_nr = faces_a
+        encoding_faces_a = encoding_faces_a_obj[vp_nr]
+        if vp_nr in list(encoding_faces_b_obj.keys()):
+            encoding_faces_b = encoding_faces_b_obj[vp_nr]
+            encoding_faces_b_obj.pop(vp_nr)
+        if vp_nr in list(encoding_words_a_obj.keys()):
+            encoding_words_a = encoding_words_a_obj[vp_nr]
+            encoding_words_a_obj.pop(vp_nr)
+        if vp_nr in list(encoding_words_b_obj.keys()):
+            encoding_words_b = encoding_words_b_obj[vp_nr]
+            encoding_words_b_obj.pop(vp_nr)
+        participants_table_dict[vp_nr] = TableBuilder.build_encoding_table_for_participant(
+            stimulus_object = stimulus_object,
+            vp_nr_a = vp_nr,
+            vp_nr_b = vp_nr,
+            encoding_faces_a = encoding_faces_a,
+            encoding_faces_b = encoding_faces_b,
+            encoding_words_a = encoding_words_a,
+            encoding_words_b = encoding_words_b,
+        )
+
+    # tables for participants, that weren't in faces_a
+    for faces_b in list(encoding_faces_b_obj.keys()):
+        encoding_faces_b, encoding_words_a, encoding_words_b = None, None, None
+        vp_nr = faces_b
+        encoding_faces_b = encoding_faces_b_obj[vp_nr]
+        encoding_faces_b_obj.pop(vp_nr)
+        if vp_nr in list(encoding_words_a_obj.keys()):
+            encoding_words_a = encoding_words_a_obj[vp_nr]
+            encoding_words_a_obj.pop(vp_nr)
+        if vp_nr in list(encoding_words_b_obj.keys()):
+            encoding_words_b = encoding_words_b_obj[vp_nr]
+            encoding_words_b_obj.pop(vp_nr)
+        participants_table_dict[vp_nr] = TableBuilder.build_encoding_table_for_participant(
+            stimulus_object = stimulus_object,
+            vp_nr_a = vp_nr,
+            vp_nr_b = vp_nr,
+            encoding_faces_a = None,
+            encoding_faces_b = encoding_faces_b,
+            encoding_words_a = encoding_words_a,
+            encoding_words_b = encoding_words_b,
+        )
+    # tables for participants, that weren't in faces_a or faces_b
+    for words_a in list(encoding_words_a_obj.keys()):
+        encoding_words_a, encoding_words_b = None, None
+        encoding_words_a = encoding_words_a_obj[vp_nr]
+        vp_nr = words_a
+        if vp_nr in list(encoding_words_b_obj.keys()):
+            encoding_words_b = encoding_words_b_obj[vp_nr]
+            encoding_words_b_obj.pop(vp_nr)
+        participants_table_dict[vp_nr] = TableBuilder.build_encoding_table_for_participant(
+            stimulus_object = stimulus_object,
+            vp_nr_a = vp_nr,
+            vp_nr_b = vp_nr,
+            encoding_faces_a = None,
+            encoding_faces_b = None,
+            encoding_words_a = encoding_words_a,
+            encoding_words_b = encoding_words_b,
+        )
+    # tables for participants, that weren't in faces_a, faces_b or words_a
+    for words_b in list(encoding_words_b_obj.keys()):
+        encoding_words_b = None
+        encoding_words_b = encoding_words_b_obj[vp_nr]
+        vp_nr = words_b
+        participants_table_dict[vp_nr] = TableBuilder.build_encoding_table_for_participant(
+            stimulus_object = stimulus_object,
+            vp_nr_a = vp_nr,
+            vp_nr_b = vp_nr,
+            encoding_faces_a = None,
+            encoding_faces_b = None,
+            encoding_words_a = None,
+            encoding_words_b = encoding_words_b,
+        )
+    return participants_table_dict
+
+# TODO: build recognition table
+def run_build_recognition_table_dfs(stimulus_object):
+    pass
 
 def run_build_table_dfs():
     stimulus_object = InputHandler.stimulus_lists_workflow()
--- src/TableBuilder.py
+++ src/TableBuilder.py
@@ -4,6 +4,7 @@ import numpy as np
 # TODO: build recognition table for participant
 # TODO: build whole table in right order
 
+# TODO: test for incomplete Data
 def build_encoding_table_for_participant(
     stimulus_object, 
     vp_nr_a = None,
@@ -18,22 +19,18 @@ def build_encoding_table_for_participant(
         vp_nr = vp_nr_a
         encoding_task =  str(vp_nr) + '_encoding_faces_a'
         participant_encoding_table_obj[encoding_task] = build_encoding_faces_table_from_df(vp_nr, encoding_faces_a, stimulus_object['df_encoding_faces_a'])
-        print('faces a finished')
     if encoding_faces_b is not None:
         vp_nr = vp_nr_b
         encoding_task =  str(vp_nr) + '_encoding_faces_b'
         participant_encoding_table_obj[encoding_task] = build_encoding_faces_table_from_df(vp_nr, encoding_faces_b, stimulus_object['df_encoding_faces_b'])
-        print('faces b finished')
     if encoding_words_a is not None:
         vp_nr = vp_nr_a
         encoding_task =  str(vp_nr)  + '_encoding_words_a'
         participant_encoding_table_obj[encoding_task] = build_encoding_words_table_from_df(vp_nr, encoding_words_a, stimulus_object['df_encoding_words_a'])
-        print('words a finished')
     if encoding_words_b is not None:
         vp_nr = vp_nr_b
         encoding_task =  str(vp_nr)  + '_encoding_words_b'
         participant_encoding_table_obj[encoding_task] = build_encoding_words_table_from_df(vp_nr, encoding_words_b, stimulus_object['df_encoding_words_b'])
-        print('words b finished')
     return participant_encoding_table_obj
 
 def build_encoding_faces_table_from_df(vp_nr, encoding_faces_df, df_stimulus_encoding_faces):
