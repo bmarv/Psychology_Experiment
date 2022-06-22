4bdc31ee5d21ea47e6f1389dc2d3d82341ba95d5
bmarvin <beesemarvin@gmail.com>
2022-06-22 11:41:27 +0200

    specify encoding df in Main for building tables more generic

--- Main.py
+++ Main.py
@@ -6,7 +6,15 @@ def run_build_encoding_table_dfs(stimulus_object):
     encoding_data_a_list = InputHandler.encoding_workflow()
     encoding_data_b_list = InputHandler.encoding_workflow()
     # TODO: make table for every participant
-    df_table_participant = TableBuilder.build_encoding_table_for_participant(stimulus_object, encoding_data_a_list[0], encoding_data_b_list[0])
+    df_table_participant = TableBuilder.build_encoding_table_for_participant(
+        stimulus_object = stimulus_object,
+        vp_nr_a = encoding_data_a_list[0]['vp_nr'],
+        vp_nr_b = encoding_data_b_list[0]['vp_nr'],
+        encoding_faces_a = encoding_data_a_list[0]['df_faces'],
+        encoding_faces_b = encoding_data_b_list[0]['df_faces'],
+        encoding_words_a = encoding_data_a_list[0]['df_words'],
+        encoding_words_b = encoding_data_b_list[0]['df_words'],
+        )
     return df_table_participant
 
 
--- src/TableBuilder.py
+++ src/TableBuilder.py
@@ -4,45 +4,64 @@ import numpy as np
 # TODO: build recognition table for participant
 # TODO: build whole table in right order
 
-def build_encoding_table_for_participant(stimulus_object, encoding_data_obj_a, encoding_data_obj_b):    
-    # index 0,1: faces a,b; index 2,3: words a,b;
-    # a for even indices, b for odd indices
-    df_stimulus_encoding_list = [
-        stimulus_object['df_encoding_faces_a'],
-        stimulus_object['df_encoding_faces_b'],
-        stimulus_object['df_encoding_words_a'],
-        stimulus_object['df_encoding_words_b']
-    ]
-
+def build_encoding_table_for_participant(
+    stimulus_object, 
+    vp_nr_a = None,
+    vp_nr_b = None,
+    encoding_faces_a = None, 
+    encoding_faces_b = None, 
+    encoding_words_a = None, 
+    encoding_words_b = None
+):
     participant_encoding_table_obj = {}
-    for index in range(0,4):
-        # encoding_data_obj for a, if index is even; b if index is odd
-        if index%2:
-            encoding_data_obj = encoding_data_obj_b
-            vp_nr = encoding_data_obj['vp_nr']
-            encoding_task = str(vp_nr) + '_b'
-        else:
-            encoding_data_obj = encoding_data_obj_a
-            vp_nr = encoding_data_obj['vp_nr']
-            encoding_task = str(vp_nr) + '_a'
-        # encoding element for faces for index 0,1; for words for index 2,3
-        if index < 2:
-            encoding_task += '_encoding_faces'
-            df_encoding_element = encoding_data_obj['df_faces']
-        else:
-            encoding_task += '_encoding_words'
-            df_encoding_element = encoding_data_obj['df_words']
-        df_encoding_element['VP'] = vp_nr
-        df_encoding_element['Stimulation'] = df_encoding_element.apply(lambda row: row.order + 1, axis = 1)
-        df_encoding_element = df_encoding_element.rename(columns={
-            'task': 'Aufgabe',
-            'valence': 'Antwort'
-        })
-        df_encoding_element['A_B'] = df_encoding_element.apply(lambda row: 'A' if (row.order == 0) else 'B', axis = 1)
-        df_encoding_element['Stimulus'] = df_encoding_element.apply(lambda row: df_stimulus_encoding_list[index].iloc[row.tablerow - 1]['name'], axis = 1)
-        df_encoding_element['Antwort_Order'] = np.nan
-        df_encoding_element = df_encoding_element[[
-            'VP', 'Stimulation', 'Aufgabe', 'A_B', 'Stimulus', 'Antwort_Order', 'Antwort', 'RT'
-        ]]
-        participant_encoding_table_obj[encoding_task] = df_encoding_element
+    if encoding_faces_a is not None:
+        vp_nr = vp_nr_a
+        encoding_task =  str(vp_nr) + '_encoding_faces_a'
+        participant_encoding_table_obj[encoding_task] = build_encoding_faces_table_from_df(vp_nr, encoding_faces_a, stimulus_object['df_encoding_faces_a'])
+        print('faces a finished')
+    if encoding_faces_b is not None:
+        vp_nr = vp_nr_b
+        encoding_task =  str(vp_nr) + '_encoding_faces_b'
+        participant_encoding_table_obj[encoding_task] = build_encoding_faces_table_from_df(vp_nr, encoding_faces_b, stimulus_object['df_encoding_faces_b'])
+        print('faces b finished')
+    if encoding_words_a is not None:
+        vp_nr = vp_nr_a
+        encoding_task =  str(vp_nr)  + '_encoding_words_a'
+        participant_encoding_table_obj[encoding_task] = build_encoding_words_table_from_df(vp_nr, encoding_words_a, stimulus_object['df_encoding_words_a'])
+        print('words a finished')
+    if encoding_words_b is not None:
+        vp_nr = vp_nr_b
+        encoding_task =  str(vp_nr)  + '_encoding_words_b'
+        participant_encoding_table_obj[encoding_task] = build_encoding_words_table_from_df(vp_nr, encoding_words_b, stimulus_object['df_encoding_words_b'])
+        print('words b finished')
     return participant_encoding_table_obj
+
+def build_encoding_faces_table_from_df(vp_nr, encoding_faces_df, df_stimulus_encoding_faces):
+    encoding_faces_df['VP'] = int(vp_nr)
+    encoding_faces_df['Stimulation'] = encoding_faces_df.apply(lambda row: row.order + 1, axis = 1)
+    encoding_faces_df = encoding_faces_df.rename(columns={
+        'task': 'Aufgabe',
+        'valence': 'Antwort'
+    })
+    encoding_faces_df['A_B'] = encoding_faces_df.apply(lambda row: 'A' if (row.order == 0) else 'B', axis = 1)
+    encoding_faces_df['Stimulus'] = encoding_faces_df.apply(lambda row: df_stimulus_encoding_faces.iloc[row.tablerow - 1]['name'], axis = 1)
+    encoding_faces_df['Antwort_Order'] = np.nan
+    encoding_faces_df = encoding_faces_df[[
+        'VP', 'Stimulation', 'Aufgabe', 'A_B', 'Stimulus', 'Antwort_Order', 'Antwort', 'RT'
+    ]]
+    return encoding_faces_df
+
+def build_encoding_words_table_from_df(vp_nr, encoding_words_df, df_stimulus_encoding_words):
+    encoding_words_df['VP'] = int(vp_nr)
+    encoding_words_df['Stimulation'] = encoding_words_df.apply(lambda row: row.order + 1, axis = 1)
+    encoding_words_df = encoding_words_df.rename(columns={
+        'task': 'Aufgabe',
+        'valence': 'Antwort'
+    })
+    encoding_words_df['A_B'] = encoding_words_df.apply(lambda row: 'A' if (row.order == 0) else 'B', axis = 1)
+    encoding_words_df['Stimulus'] = encoding_words_df.apply(lambda row: df_stimulus_encoding_words.iloc[row.tablerow - 1]['name'], axis = 1)
+    encoding_words_df['Antwort_Order'] = np.nan
+    encoding_words_df = encoding_words_df[[
+        'VP', 'Stimulation', 'Aufgabe', 'A_B', 'Stimulus', 'Antwort_Order', 'Antwort', 'RT'
+    ]]
+    return encoding_words_df
